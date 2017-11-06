c6c419226b84d1b020f308d7ae418b6642f6ac7f
Alexander Demin <bouvens@gmail.com>
2017-11-06 21:26:26 +0600

    Initial commit

---
+++ src/Canvas.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import _ from 'lodash'
+
+export default class Canvas extends React.PureComponent {
+    static propTypes = {
+        width: PropTypes.number.isRequired,
+        height: PropTypes.number.isRequired,
+        field: PropTypes.arrayOf(PropTypes.array).isRequired,
+        states: PropTypes.number.isRequired,
+    }
+
+    componentDidMount () {
+        this.paint()
+    }
+
+    componentDidUpdate () {
+        this.paint()
+    }
+
+    canvasContext
+    canvasData
+
+    drawPixel = (x, y, r, g, b) => {
+        const index = (x + (y * this.props.width)) * 4
+
+        this.canvasData.data[index] = r
+        this.canvasData.data[index + 1] = g
+        this.canvasData.data[index + 2] = b
+        this.canvasData.data[index + 3] = 255
+    }
+
+    clear = () => {
+        this.canvasContext.clearRect(0, 0, this.props.width, this.props.height)
+    }
+
+    paint = (field = this.props.field) => {
+        this.canvasData = this.canvasContext.getImageData(0, 0, this.props.width, this.props.height)
+
+        for (let x = 0; x < this.props.width; x += 1) {
+            for (let y = 0; y < this.props.height; y += 1) {
+                this.drawPixel(x, y, _.get(field, [x, y]) / this.props.states)
+            }
+        }
+
+        this.canvasContext.putImageData(this.canvasData, 0, 0)
+    }
+
+    refCanvas = (elem) => {
+        this.canvasContext = elem.getContext('2d')
+    }
+
+    render () {
+        return (
+            <canvas
+                ref={this.refCanvas}
+                width={this.props.width}
+                height={this.props.height}
+            >
+                {'You are using an outdated browser without support of canvas elements.'}
+            </canvas>
+        )
+    }
+}
---
+++ src/Experiment.jsx
@@ -0,0 +1,213 @@
+import React, { Component } from 'react'
+import _ from 'lodash'
+import { Connector, Input, SettersBlock } from 'state-control'
+
+const CANVAS_SIZE = 10
+
+const IDS = {
+    multiplier: 'multiplier',
+}
+
+const setters = {
+    Default: {
+        [IDS.multiplier]: 1,
+    },
+}
+
+const getColorsFromImage = ({ data, width, height }) => {
+    const mapRGB = []
+
+    for (let y = 0; y < height; y += 1) {
+        for (let x = 0; x < width; x += 1) {
+            const index = ((y * width) + x) * 4
+            const [r, g, b] = data.slice(index, index + 3)
+
+            _.set(mapRGB, [x, y], { r, g, b })
+        }
+    }
+
+    return mapRGB
+}
+
+export default class Experiment extends Component {
+    // noinspection JSUnusedGlobalSymbols
+    static defaultProps = {
+        ...setters.Default,
+    }
+
+    constructor (props) {
+        super(props)
+
+        this.state = {
+            ...this.props,
+            output: '',
+        }
+
+        this.file = new FileReader()
+        this.file.onload = this.onFileLoad
+        this.file.onerror = this.onError
+
+        this.img = new Image()
+        this.img.onload = this.onImageLoad
+        this.img.onerror = this.onError
+
+        this.offscreenCanvas = document.createElement('canvas')
+        this.offscreenContext = this.offscreenCanvas.getContext('2d')
+
+        this.mapRGB = []
+    }
+
+    componentDidMount () {
+        this.img.src = './sample.jpg'
+    }
+
+    onFileLoad = ({ target }) => {
+        this.img.src = target.result
+    }
+
+    onImageLoad = ({ target }) => {
+        this.offscreenCanvas.width = target.width
+        this.offscreenCanvas.height = target.height
+        this.offscreenContext.drawImage(target, 0, 0)
+
+        this.mapRGB = getColorsFromImage(this.offscreenContext.getImageData(0, 0, target.width, target.height))
+        this.drawRGB()
+    }
+
+    onError = (error) => {
+        debugger
+        this.setState({ output: error })
+    }
+
+    canvas
+    canvasContext
+
+    drawRGB = () => {
+        const { width, height } = this.offscreenCanvas
+        const newWidth = width * 3
+        const newHeight = height * 3
+        this.offscreenCanvas.width = newWidth
+        this.offscreenCanvas.height = newHeight
+        const imageData = this.offscreenContext.getImageData(0, 0, newWidth, newHeight)
+        const { data } = imageData
+
+        for (let y = 0; y < height; y += 1) {
+            let newLine = []
+
+            for (let x = 0; x < width; x += 1) {
+                const { r, g, b } = this.mapRGB[x][y]
+                const red = [r, 0, 0, 255]
+                const green = [0, g, 0, 255]
+                const blue = [0, 0, b, 255]
+                const allTogether = _.concat(red, green, blue)
+
+                newLine = newLine.concat(allTogether)
+            }
+
+            data.set(_.concat(newLine, newLine, newLine), (y * width * 3) * 4 * 3)
+        }
+
+        this.offscreenContext.putImageData(imageData, 0, 0)
+
+        this.canvas.width = newWidth * this.state.multiplier
+        this.canvas.height = newHeight * this.state.multiplier
+        this.canvasContext.imageSmoothingEnabled = false
+        this.canvasContext.scale(this.state.multiplier, this.state.multiplier)
+        this.canvasContext.drawImage(this.offscreenCanvas, 0, 0)
+    }
+
+    paintOffscreen = () => {
+        this.canvas.width = this.offscreenCanvas.width
+        this.canvas.height = this.offscreenCanvas.height
+        this.canvasContext.drawImage(this.offscreenCanvas, 0, 0)
+    }
+
+    handleFileSelect = (evt) => {
+        evt.stopPropagation()
+        evt.preventDefault()
+
+        const { files } = evt.dataTransfer
+
+        const output = _.map(files, (f) => (
+            <li key={f.name}>
+                <strong>{f.name}</strong> ({f.type || 'n/a'}) - {f.size} bytes, last
+                modified: {f.lastModifiedDate.toLocaleDateString()}
+            </li>
+        ))
+
+        this.file.readAsDataURL(files[0])
+
+        this.setState({ output })
+    }
+
+    handleDragOver = (evt) => {
+        evt.stopPropagation()
+        evt.preventDefault()
+        evt.dataTransfer.dropEffect = 'copy' // explicitly show this is a copy
+    }
+
+    changeHandler = (name, value) => {
+        this.setState({ [name]: value })
+    }
+
+    selectAll = (control) => control.setSelectionRange(0, control.value.length)
+
+    refCanvas = (elem) => {
+        this.canvas = elem
+        this.canvasContext = elem.getContext('2d')
+        this.canvas.width = CANVAS_SIZE
+        this.canvas.height = CANVAS_SIZE
+    }
+
+    render () {
+        return (
+            <div>
+                <SettersBlock
+                    setters={setters}
+                    setHandler={this.changeHandler}
+                    key="setters"
+                />
+                <Connector
+                    state={this.state}
+                    onChange={this.changeHandler}
+                    onFocus={this.selectAll}
+                    className="state-control-input"
+                    key="connector"
+                >
+                    <Input
+                        id={IDS.multiplier}
+                        label="Multiplier"
+                        defaultNum={1}
+                    />
+                </Connector>
+                <hr />
+                <div
+                    onDragOver={this.handleDragOver}
+                    onDrop={this.handleFileSelect}
+                    style={{
+                        border: '2px dashed #bbb',
+                        borderRadius: '15px',
+                        padding: '25px',
+                        textAlign: 'center',
+                        fontSize: '20px',
+                        fontWeight: 'bold',
+                        fontFamily: 'sans-serif',
+                        color: '#bbb',
+                    }}
+                >
+                    Drop file here
+                </div>
+                <canvas
+                    ref={this.refCanvas}
+                    style={{
+                        margin: '1.5em 0',
+                        backgroundColor: 'green',
+                    }}
+                >
+                    {'You are using an outdated browser without support of canvas elements.'}
+                </canvas>
+                <div>{this.state.output}</div>
+            </div>
+        )
+    }
+}
