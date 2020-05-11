c5fb3b8e041858aceea2cdb0091dab1681eeb5f7
Alexander Demin <bouvens@gmail.com>
2020-05-11 14:51:24 +0600

    refactor: Simplify common index.js

--- src/common/index.js
+++ src/common/index.js
@@ -1,9 +1,3 @@
 export { toRGB } from './image-processing'
-export {
-  getImageFromSrc,
-  getDivider,
-  PARAMETER_PROCESSORS,
-  getDeviation,
-} from './utils'
-export { getCanvas, getContext } from './singletons'
+export { getImageFromSrc, PARAMETER_PROCESSORS } from './utils'
 export { IDS, SETTERS, THROBBER, SAMPLE_IMAGE_PATHS } from './constants'
